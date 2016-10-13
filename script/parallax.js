81f5db36a7396800f708655448d1ceace613bef3
ughe <ughe@users.noreply.github.com>
2016-10-13 19:43:51 -0400

    Changed header

--- script/parallax.js
+++ script/parallax.js
@@ -1,10 +1,21 @@
 const FADE_TITLE = 25/100;  // percent of screen
+const CHANGE_MENU = 62/100;
 
-$(window).scroll( function() {
-    if ( $(window).scrollTop() < $( window ).height() * FADE_TITLE ) {
-        $(".title").fadeIn();
-    } else {
-        $(".title").fadeOut();
+$(window).scroll(
+    function() {
+        var scrollTop = $( window ).scrollTop()
+        var windowHeight = $( window ).height()
+
+        if ( scrollTop < windowHeight * FADE_TITLE ) {
+            $(".title").fadeIn(100);
+        } else {
+            $(".title").fadeOut(500);
+        }
+
+        if ( scrollTop < windowHeight * CHANGE_MENU ) {
+            $(".menu").css("background-color","rgba(0, 0, 0, .25)");
+        } else {
+            $(".menu").css("background-color","#3E4040");
+        }
     }
-} );
-
+);
